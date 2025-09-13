70cb07fa9d45132f7338f376c437377aeeb0cd25
alaa03omari <omari2003aloosh@gmail.com>
2025-09-13 18:51:29 +0300

    first commit t

---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿let btn = document.getElementById('save-btn');
+let container = document.getElementById('container');
+let emailInput = document.getElementById('email-input');
+let tooltiptext = document.getElementById('tooltiptext');
+
+function addInputHint() {
+    tooltiptext.style.visibility ='visible'
+}
+function removeInputHint() {
+    tooltiptext.style.visibility='hidden'
+}
+function handleSaveBtn() {
+    container.style.backgroundColor = "#a078b3"
+}
+
+btn.addEventListener('click', handleSaveBtn)
+emailInput.addEventListener("mouseover", addInputHint)
+emailInput.addEventListener("mouseleave", removeInputHint)
